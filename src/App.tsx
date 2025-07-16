5914f0c53cd4665b751a0121577f0360bd2cd0a2
Mateo <mroldantaborda1@gmail.com>
2025-07-15 21:20:06 -0500

    Add setCompletedTodos state, installed pragmatic dnd

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -27,7 +28,13 @@ const App: React.FC = () => {
 
       <span className="heading">Taskify</span>
       <InputField todo = {todo} setTodo = {setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <TodoList 
+        todos={todos} 
+        setTodos={setTodos}
+        completedTodos={completedTodos}
+        setCompletedTodos={setCompletedTodos}
+      />
+      
       </div>
     </>
   )
